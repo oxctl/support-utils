16b7dac966203cd3035ea6f77424afafe858bbe1
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2020-05-27 11:50:12 +0100

    Fix for setting exiting after one update.

--- course-index/manage-course-index.sh
+++ course-index/manage-course-index.sh
@@ -28,7 +28,7 @@ csvcut -c canvas_course_id | tail +2| while read id; do
     echo -n "$id,"
     ccurl -s https://${host}/api/v1/courses/${id}?include[]=indexed | jq '.indexed'
     if [ -n "$setting" ]; then
-      ccurl -s -X PUT -Fcourse[indexed]=${setting} -o /dev/null https://${host}/api/v1/courses/${id} || echo "Failed to update ${id}" 1>&2; exit 1
+      ccurl -s -X PUT -Fcourse[indexed]=${setting} -o /dev/null https://${host}/api/v1/courses/${id} || (echo "Failed to update ${id}" 1>&2; exit 1)
     fi
 
 done

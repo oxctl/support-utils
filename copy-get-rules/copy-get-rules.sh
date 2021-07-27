ebefd2bad35ef56345ecb35074908b5f9bf33f10
nicholaswilson100 <nicholaswilson100@gmail.com>
2021-07-27 12:24:51 +0100

    Test and Beta versions

--- copy-get-rules/copy-get-rules.sh
+++ copy-get-rules/copy-get-rules.sh
@@ -4,21 +4,22 @@
 # Needs cut, ccurl, iconv installed.
 
 if [ "$1" == "" ]; then
-  echo Usage: $(basename $0) live_token beta_token test_token;
+  echo Usage: $(basename $0) live_token beta_token test_token enviroment;
   exit 1;
 fi
 
 live_token="$1";
 beta_token="$2";
 test_token="$3";
+env="$4";
 
-if [ -z "${live_token}" ] || [ -z "${beta_token}" ] || [ -z "${test_token}" ]; then
-  echo "You must set live_token, beta_token and test_token for this to work."
+if [ -z "${live_token}" ] || [ -z "${beta_token}" ] || [ -z "${test_token}" ] || [ -z "${env}" ]; then
+  echo "You must set live_token, beta_token and test_token and the environment for this to work."
   exit 1
 fi
 
 # Send start email
-echo "Subject: ***Copy GET rules process***: Starting process to copy GET rules from production to test and beta" | sendmail nick.wilson@it.ox.ac.uk;
+echo "Subject: ***Copy GET rules process*** ($env version): Starting process to copy GET rules from production to test and beta" | sendmail nick.wilson@it.ox.ac.uk;
 
 echo "Live token is" $live_token;
 echo "beta token is"  $beta_token;
@@ -28,7 +29,6 @@ echo "Test token is" $test_token;
 echo "Exporting the GET rules from production\n";
 job_id=$(curl --location --request POST 'https://canvas-group-enrollment-dub-prod.insproserv.net/export/rules' --header 'Authorization: '$live_token  | grep -Po 'job_id":\K[0-9]+' )
 statusUrl=https://canvas-group-enrollment-dub-prod.insproserv.net/status/$job_id;
-echo "Job id is" $job_id;
 
 # Test for job id
 if [ -z "$job_id" ]  ; then
@@ -88,10 +88,19 @@ if [ "$exportCompleted" = true ] ; then
 	cut -d, -f2 --complement rule_groups1.csv > rule_groups2.csv;
 	cut -d, -f2 --complement rule_courses.csv > rule_courses1.csv;
 
-	# This section is for testing changes
-	#head -n 2 "rules1.csv" >"rules.csv";
-	#head -n 2 "rule_groups1.csv" >"rule_groups.csv";
-	#head -n 2 "rule_courses1.csv" >"rule_courses.csv";
+	# If test or beta, cut down files if test or beta
+	if [ "$env" = 'test' ] || [ "$env" = 'beta' ]  ; then
+		echo "Subject: ***Copy GET rules process*** ($env version): Cutting files down" | sendmail nick.wilson@it.ox.ac.uk;
+
+		# This section is for testing changes
+		head -n 2 "rules1.csv" >"rules_test.csv";
+		head -n 2 "rule_groups2.csv" >"rule_groups_test.csv";
+		head -n 2 "rule_courses1.csv" >"rule_courses_test.csv";
+
+		mv rules_test.csv rules1.csv;
+		mv rule_groups_test.csv rule_groups2.csv;
+		mv rule_courses_test.csv rule_courses1.csv;
+	fi
 
 	# Rename column
 	sed -i '1s/rule_id/rule_import_id/'  rules1.csv;
@@ -108,103 +117,114 @@ if [ "$exportCompleted" = true ] ; then
 	zip -r $zipToImport rules2.csv rule_groups3.csv rule_courses2.csv;
 
 	# Importing rules to test...
-	echo "Importing rules to test...";
-	import_job_id=$(curl --location --request POST 'https://canvas-group-enrollment-dub-test.insproserv.net/import/rules' \
-	--header 'Authorization: '$test_token --form 'file=@'$zipToImport | grep -Po 'job_id":\K[0-9]+' );
-	statusImportUrl=https://canvas-group-enrollment-dub-test.insproserv.net/status/$import_job_id;
+	if [ "$env" = 'test' ] || [ "$env" = 'prod' ]  ; then
+        	echo "Importing rules to test...";
+		import_job_id=$(curl --location --request POST 'https://canvas-group-enrollment-dub-test.insproserv.net/import/rules' \
+		--header 'Authorization: '$test_token --form 'file=@'$zipToImport | grep -Po 'job_id":\K[0-9]+' );
+		statusImportUrl=https://canvas-group-enrollment-dub-test.insproserv.net/status/$import_job_id;
 
-	# Test for job id
-    if [ -z "import_job_id" ]  ; then
-        echo "Job id empty so exiting....";
-        exit 1;
-    fi
+		# Test for job id
+		    if [ -z "import_job_id" ]  ; then
+			echo "Job id empty so exiting....";
+			exit 1;
+		    fi
 
-	# Check if import has finished
-	echo "import job id: " $import_job_id;
-	runtime="60 minute"
-	endtime=$(date -ud "$runtime" +%s)
-	while [[ $(date -u +%s) -le $endtime ]]
-	do
-		echo "Job id is" $import_job_id;
-		echo "Time Now: `date +%H:%M:%S`"
-    		echo "Sleeping for 30 seconds"
-    		sleep 30
-    		importStatus=$(curl --location --request GET $statusImportUrl --header 'Authorization: '$test_token | sed -n 's|.*"status":"\([^"]*\)".*|\1|p' )
-		if [ $importStatus == "completed" ]; 	then
-			echo "Success! Import to test completed";
-			importCompleted=true;
-			break;
-    		fi
+		# Check if import has finished
+		echo "import job id: " $import_job_id;
+		runtime="60 minute"
+		endtime=$(date -ud "$runtime" +%s)
+		while [[ $(date -u +%s) -le $endtime ]]
+		do
+			echo "Job id is" $import_job_id;
+			echo "Time Now: `date +%H:%M:%S`"
+	    		echo "Sleeping for 30 seconds"
+	    		sleep 30
+	    		importStatus=$(curl --location --request GET $statusImportUrl --header 'Authorization: '$test_token | sed -n 's|.*"status":"\([^"]*\)".*|\1|p' )
+			if [ $importStatus == "completed" ]; 	then
+				echo "Success! Import to test completed";
+				importCompleted=true;
+				break;
+	    		fi
 
-		if [ $importStatus == "failed" ]; 	then
-			echo "Failure! Import to test not fully completed";
-			break;
-    		fi
-   		echo "Import to test not processed yet - trying again in 30 seconds..."
-	done
+			if [ $importStatus == "failed" ]; 	then
+				echo "Failure! Import to test not fully completed";
+				break;
+	    		fi
+	   		echo "Import to test not processed yet - trying again in 30 seconds..."
+		done
 
-	# Exit if import to test not complete within 60 mins
-	if [ "$importCompleted" != true ] ; then
-		echo "Import to test not complete within 60 minutes so exiting with failure...";
-		echo "Subject: ***Copy GET rules process***: Failed to imported rules to Canvas Test" | sendmail nick.wilson@it.ox.ac.uk;
-		exit 1;
-	fi
+		# Exit if import to test not complete within 60 mins
+		if [ "$importCompleted" != true ] ; then
+			echo "Import to test not complete within 60 minutes so exiting with failure...";
+			echo "Subject: ***Copy GET rules process*** ($env version): Failed to imported rules to Canvas Test" | sendmail nick.wilson@it.ox.ac.uk;
+			exit 1;
+		fi
 
-	# Send success email
-	if [ "$importCompleted" = true ] ; then
-		echo "Import to test complete within 60 minutes";
-		echo "Subject: ***Copy GET rules process***: Imported rules to Canvas Test" | sendmail nick.wilson@it.ox.ac.uk;
-	fi
+		# Send success email
+		if [ "$importCompleted" = true ] ; then
+			echo "Import to test complete within 60 minutes";
+			echo "Subject: ***Copy GET rules process*** ($env version): Imported rules to Canvas Test" | sendmail nick.wilson@it.ox.ac.uk;
+		fi
+    	fi
 
+
+
+	
 	# Importing rules to beta...
-	echo "Importing rules to beta...";
-	import_job_id_beta=$(curl --location --request POST 'https://canvas-group-enrollment-dub-test.insproserv.net/import/rules' --header 'Authorization: '$beta_token --form 'file=@'$zipToImport | grep -Po 'job_id":\K[0-9]+' );
-	statusImportUrl=https://canvas-group-enrollment-dub-test.insproserv.net/status/$import_job_id_beta;
+	if [ "$env" = 'beta' ] || [ "$env" = 'prod' ]  ; then
 
-	# Test for job id
-    if [ -z "import_job_id" ]  ; then
-        echo "Job id empty so exiting....";
-        exit 1;
-    fi
 
-	# Check if import to beta has finished
-	echo "import job id beta: " $import_job_id_beta;
-	runtime="60 minute"
-	endtime=$(date -ud "$runtime" +%s)
-	while [[ $(date -u +%s) -le $endtime ]]
-	do
-		echo "Job id is" $import_job_id_beta;
-		echo "Time Now: `date +%H:%M:%S`"
-    		echo "Sleeping for 30 seconds"
-    		sleep 30
-    		importStatus=$(curl --location --request GET $statusImportUrl --header 'Authorization: '$beta_token | sed -n 's|.*"status":"\([^"]*\)".*|\1|p')
-		if [ $importStatus == "completed" ]; 	then
-			echo "Success! Import to beta completed";
-			importCompleted=true;
-			break;
-    		fi
 
-		if [ $importStatus == "failed" ]; 	then
-			echo "Failure! Import to beta not fully completed";
-			break;
-    		fi
-   		echo "Import to beta not processed yet - trying again in 30 seconds..."
-	done
+		# Importing rules to beta...
+		echo "Importing rules to beta...";
+		import_job_id_beta=$(curl --location --request POST 'https://canvas-group-enrollment-dub-test.insproserv.net/import/rules' --header 'Authorization: '$beta_token --form 'file=@'$zipToImport | grep -Po 'job_id":\K[0-9]+' );
+		statusImportUrl=https://canvas-group-enrollment-dub-test.insproserv.net/status/$import_job_id_beta;
 
-	# Exit if import not complete
-	if [ "$importCompleted" != true ] ; then
-		echo "Import to beta not complete within 60 minutes so exiting with failure...";
-		echo "Subject: ***Copy GET rules process***: Failed to imported rules to Canvas Beta" | sendmail nick.wilson@it.ox.ac.uk;
+		# Test for job id
+	    if [ -z "import_job_id" ]  ; then
+		echo "Job id empty so exiting....";
 		exit 1;
-	fi
+	    fi
 
-	# Send success email
-	if [ "$importCompleted" = true ] ; then
-		echo "Import to beta complete within 60 minutes";
-		echo "Subject: ***Copy GET rules process***: Imported rules to Canvas Beta" | sendmail nick.wilson@it.ox.ac.uk;
+		# Check if import to beta has finished
+		echo "import job id beta: " $import_job_id_beta;
+		runtime="60 minute"
+		endtime=$(date -ud "$runtime" +%s)
+		while [[ $(date -u +%s) -le $endtime ]]
+		do
+			echo "Job id is" $import_job_id_beta;
+			echo "Time Now: `date +%H:%M:%S`"
+	    		echo "Sleeping for 30 seconds"
+	    		sleep 30
+	    		importStatus=$(curl --location --request GET $statusImportUrl --header 'Authorization: '$beta_token | sed -n 's|.*"status":"\([^"]*\)".*|\1|p')
+			if [ $importStatus == "completed" ]; 	then
+				echo "Success! Import to beta completed";
+				importCompleted=true;
+				break;
+	    		fi
+
+			if [ $importStatus == "failed" ]; 	then
+				echo "Failure! Import to beta not fully completed";
+				break;
+	    		fi
+	   		echo "Import to beta not processed yet - trying again in 30 seconds..."
+		done
+
+		# Exit if import not complete
+		if [ "$importCompleted" != true ] ; then
+			echo "Import to beta not complete within 60 minutes so exiting with failure...";
+			echo "Subject: ***Copy GET rules process*** ($env version): Failed to imported rules to Canvas Beta" | sendmail nick.wilson@it.ox.ac.uk;
+			exit 1;
+		fi
+
+		# Send success email
+		if [ "$importCompleted" = true ] ; then
+			echo "Import to beta complete within 60 minutes";
+			echo "Subject: ***Copy GET rules process*** ($env version): Imported rules to Canvas Beta" | sendmail nick.wilson@it.ox.ac.uk;
+		fi
 	fi
 fi
 
 echo "Finished imports successfully";
 # Sending starting email
-echo "Subject: ***Copy GET rules process***: Finished process successfully to copy GET rules from production to test and beta" | sendmail nick.wilson@it.ox.ac.uk;
+echo "Subject: ***Copy GET rules process*** ($env version): Finished process successfully to copy GET rules from production to test and beta" | sendmail nick.wilson@it.ox.ac.uk;
